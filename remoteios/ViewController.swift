5120530a38988fa854048fa3960e27d4bb22b1cc
Hasanul Isyraf <hasanul2307@gmail.com>
2017-03-21 19:31:07 +0800

    Initial Commit

---
+++ remoteios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remoteios
+//
+//  Created by Hasanul Isyraf on 21/03/2017.
+//  Copyright © 2017 Hasanul Isyraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
